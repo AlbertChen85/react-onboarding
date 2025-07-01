dea3465afd15f08dc09aeeac9ba3376594c0df04
Chian <>
2025-07-01 18:40:28 +0800

    feat(SOVA-2385): add todo-list/entry page

---
+++ src/app/todo-list/entry/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Box from '@mui/material/Box'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Box mt={8} display="flex" justifyContent="center" alignItems="top" minHeight="100vh">
+      <h1>This is Entry</h1>
+    </Box>
+  )
+}
+
+export default page
