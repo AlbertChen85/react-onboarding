e48974d1f0f41cb071c101481b07c2a054163773
Chian <>
2025-07-03 17:59:19 +0800

    style(SOVA-2385): update format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import HeaderBar from './headerBar';
+import { HeaderBar } from '@/components/header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
--- src/app/todo-list/page.tsx
+++ src/app/todo-list/page.tsx
@@ -1,28 +1,27 @@
-"use client";
+'use client';
 
-import React from 'react'
-import CheckboxList from './checkboxList'
-import Container from '@mui/material/Container'
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import { useRouter } from 'next/navigation'
+import React from 'react';
+import CheckboxList from './checkboxList';
+import {Box, Button, Container} from '@mui/material';
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
-    return (
-        page()
-    )
+  return page();
 }
 
 const page = () => {
-    const router = useRouter();
-    return (
-        <Container>
-            <Box mt={8} p={4} bgcolor="#ffffff" borderRadius={2}>
-                <CheckboxList />
-            </Box>
-            <Box display="flex" justifyContent="center" mt={2}>
-                <Button variant='contained' onClick={() => router.push('/todo-list/entry')}> To Entry </Button>
-            </Box>
-        </Container>
-    )
-}
+  const router = useRouter();
+  return (
+    <Container>
+      <Box mt={8} p={4} bgcolor="#ffffff" borderRadius={2}>
+        <CheckboxList />
+      </Box>
+      <Box display="flex" justifyContent="center" mt={2}>
+        <Button variant="contained" onClick={() => router.push('/todo-list/entry')}>
+          {' '}
+          To Entry{' '}
+        </Button>
+      </Box>
+    </Container>
+  );
+};
