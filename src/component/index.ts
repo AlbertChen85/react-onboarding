4d97a4a06188ee250b4bd3afa082a02ad1f710ad
Chian <>
2025-07-05 17:44:55 +0800

    feat(SOVA-2452): add zod in index for create task schema

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,2 +1,15 @@
+import { z } from 'zod';
+
 export { StyledToggleButton } from './styled-toggle-button';
 export { HeaderBar } from './headerBar';
+export { Field } from './hook-form/fields';
+
+export const createTaskFormSchema = z.object({
+  name: z.string().min(1, { message: 'Name is required.' }),
+  statusId: z.number({
+    errorMap: () => ({ message: 'Status is required.' }),
+  }),
+  description: z.string().min(1, { message: 'Description is required.' }),
+});
+
+export type CreateTaskFormSchemaType = z.infer<typeof createTaskFormSchema>;
