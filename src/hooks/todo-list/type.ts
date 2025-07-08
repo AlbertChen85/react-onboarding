ae03c8a65df564118afbb8ee892264b10e28ff2e
Chian <>
2025-07-08 15:25:51 +0800

    feat(SOVA-2474): update get tasks api with query and const type

--- src/hooks/todo-list/type.ts
+++ src/hooks/todo-list/type.ts
@@ -1,4 +1,16 @@
 export type Status = {
   id: number;
   name: string;
+
+export type TaskRow = {
+  name: string;
+  statusId: number;
+  description: string;
+};
+
+export type TaskResponse = {
+  data: TaskRow[];
+  page: number;
+  size: number;
+  total: number;
 };
