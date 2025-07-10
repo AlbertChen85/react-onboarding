ae068ac9711a98196f69251272f86874f65214b5
Albert <140475206+AlbertChen85@users.noreply.github.com>
2025-07-10 11:08:00 +0800

    Revert "feat(SOVA-2452): implement task list display and pagination"

--- src/hooks/todo-list/type.ts
+++ src/hooks/todo-list/type.ts
@@ -1,17 +1,4 @@
 export type Status = {
   id: number;
   name: string;
-}
-
-export type TaskRow = {
-  name: string;
-  statusId: number;
-  description: string;
-};
-
-export type TaskResponse = {
-  data: TaskRow[];
-  page: number;
-  size: number;
-  total: number;
 };
